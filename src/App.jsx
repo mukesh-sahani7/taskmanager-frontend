dc4443dc7adf92f1e4d538849abbc5a01a082f74
Mukesh Sahani <ms6148832@gmail.com>
2026-07-03 23:56:56 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 function App() {
   
-  const BASE_URL = 'https://taskmanager-backend-docker.onrender.com/api/auth';
+  const BASE_URL = 'https://taskmanager-backend-xll3.onrender.com/api/auth';
   // Navigation State: 'LOGIN', 'SIGNUP', ya 'DASHBOARD'
   const [page, setPage] = useState('INDEX');
 
